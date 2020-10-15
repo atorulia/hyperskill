09a7472edcb4ff566045e0cf0733e286a7034903
atorulia <33317367+atorulia@users.noreply.github.com>
2020-10-15 09:39:15 +0200

    Update settings.py

--- Hyper Service Center/hypercar/hypercar/settings.py
+++ Hyper Service Center/hypercar/hypercar/settings.py
@@ -10,7 +10,7 @@ QUEUE_JSON_PATH = os.path.join(BASE_DIR, 'queue.json')
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_^m1$hfk6a^^)zbdize#nwxya@kc9q^@$kzpzzwz!_+l#(5y0%'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
