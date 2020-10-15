c791e18bac7cb0b10b7e25a70400d9cb77b449d7
atorulia <33317367+atorulia@users.noreply.github.com>
2020-10-15 09:40:07 +0200

    Update settings.py

--- HyperJob Agency/hyperjob/hyperjob/settings.py
+++ HyperJob Agency/hyperjob/hyperjob/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*57#^8!%%x4wi6byh%qj3!&7b)qu5ve7m5tz9&)2^jievj2j1b'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
